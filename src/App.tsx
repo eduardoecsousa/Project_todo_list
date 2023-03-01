7e52a94b3ee7017ee90ab9c84db8f9f290a64890
eduardoSousa <dudus.sousa4002@gmail.com>
2023-03-01 19:41:21 -0300

    Inserido um modal para fazer a ediição de uma tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,47 @@ import styles from './App.module.css';
 import TaskForm from './components/form/TaskForm';
 import TaskList from './components/list/TaskList';
 import { ITask } from './interface/task';
+import Modal from './components/modal/Modal';
 
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([]);
 
+  const deleteTask = (id:number) => {
+    setTaskList(
+      taskList.filter((task) => task.id !== id)
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = ():void => {
+    hideOrShowModal(true);
+  };
+
   return (
     <div>
+      <Modal children={<TaskForm btnText="Editar Tafera" taskList={taskList} /> } />
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer ?</h2>
-          <TaskForm btnText="createTask" taskList={taskList} setTaskList={setTaskList} />
+          <TaskForm btnText="Adicionar tarefa" taskList={taskList} setTaskList={setTaskList} />
         </div>
         <div>
           <h2>Suas Tarefas:</h2>
-          <TaskList taskList={taskList} />
+          <TaskList
+            taskList={taskList}
+            handleDelete={ deleteTask }
+            handleEdit={ editTask }
+          />
         </div>
       </main>
       <Footer />
