ae5e895a862064578569d20edf47b3c727e9bae8
eduardoSousa <dudus.sousa4002@gmail.com>
2023-03-01 19:56:19 -0300

    implementado função para preencher os campos de input ao clicar em iditar

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Modal from './components/modal/Modal';
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask| null>(null)
 
   const deleteTask = (id:number) => {
     setTaskList(
@@ -26,13 +27,14 @@ function App() {
     }
   };
 
-  const editTask = ():void => {
+  const editTask = (task:ITask):void => {
     hideOrShowModal(true);
+    setTaskToUpdate(task);
   };
 
   return (
     <div>
-      <Modal children={<TaskForm btnText="Editar Tafera" taskList={taskList} /> } />
+      <Modal children={<TaskForm btnText="Editar Tafera" taskList={taskList} task={taskToUpdate} /> } />
       <Header />
       <main className={styles.main}>
         <div>
